df3ae9c47fe3d0fe95317a03a654b7c944ec0b34
mrVinzcode <137170998+mrVinzcode@users.noreply.github.com>
2024-02-24 03:51:07 +0800

    revised experience section

--- src/pages/Experience/Experience.jsx
+++ src/pages/Experience/Experience.jsx
@@ -26,38 +26,53 @@ const Experince = () => {
                   >
                     <span className="absolute -inset-x-4 -inset-y-2.5 hidden rounded md:-inset-x-6 md:-inset-y-4 lg:block"></span>
                     <span className="inline-block">
-                      Web Developer • Zuellig Pharma
+                      Transport Planner • Zuellig Pharma
                     </span>
                   </a>
                 </h3>
 
                 <p className="mt-2 text-sm leading-normal">
-                  As a developer, I am tasked to create and develop websites and
-                  applications for the company. To achieve this goal, I mainly
-                  use React library and its framework — Nextjs. Additionally we
-                  also use WordPress to cater clients whom prefers to create
-                  their website on WordPress CMS. We are also venturing the
-                  theme and plugin development.
+                  Executing orders in the SAP system, we assess total volume and
+                  weight. Determining the required trucks based on package
+                  dimensions, we coordinate with third-party services for
+                  pickup. We stay in communication and provide support until
+                  successful delivery to the customer.
                 </p>
+
                 <ul className="mt-2 flex flex-wrap">
                   <li className="mr-1.5 mt-2">
                     <div className="flex items-center bg-teal-400/10 rounded-full px-3 py-1 text-xs font-medium leading-5 text-teal-300">
-                      Javascript
+                      SAP EWM
                     </div>
                   </li>
                   <li className="mr-1.5 mt-2">
                     <div className="flex items-center bg-teal-400/10 rounded-full px-3 py-1 text-xs font-medium leading-5 text-teal-300">
-                      Javascript
+                      Sharepoint
                     </div>
                   </li>
                   <li className="mr-1.5 mt-2">
                     <div className="flex items-center bg-teal-400/10 rounded-full px-3 py-1 text-xs font-medium leading-5 text-teal-300">
-                      Javascript
+                      Spreadsheet
                     </div>
                   </li>
                   <li className="mr-1.5 mt-2">
                     <div className="flex items-center bg-teal-400/10 rounded-full px-3 py-1 text-xs font-medium leading-5 text-teal-300">
-                      Javascript
+                      Zyllem
+                    </div>
+                  </li>
+                  <li className="mr-1.5 mt-2">
+                    <div className="flex items-center bg-teal-400/10 rounded-full px-3 py-1 text-xs font-medium leading-5 text-teal-300">
+                      Viber
+                    </div>
+                  </li>
+                  <li className="mr-1.5 mt-2">
+                    <div className="flex items-center bg-teal-400/10 rounded-full px-3 py-1 text-xs font-medium leading-5 text-teal-300">
+                      Microsoft Teams
+                    </div>
+                  </li>
+                  <li className="mr-1.5 mt-2">
+                    <div className="flex items-center bg-teal-400/10 rounded-full px-3 py-1 text-xs font-medium leading-5 text-teal-300">
+                      Microsoft Teams
                     </div>
                   </li>
                 </ul>
@@ -68,7 +83,7 @@ const Experince = () => {
             <div className="group relative grid pb-1 transition-all sm:grid-cols-8 sm:gap-8 md:gap-4 lg:hover:!opacity-100 lg:group-hover/list:opacity-50">
               <div className="absolute -inset-x-4 -inset-y-4 z-0 hidden rounded-md transition motion-reduce:transition-none lg:-inset-x-6 lg:block lg:group-hover:bg-slate-800/50 lg:group-hover:shadow-[inset_0_1px_0_0_rgba(148,163,184,.0.1)] lg:group-hover:drop-shadow-lg"></div>
               <header className="z-10 mb-2 mt-1 text-xs font-semibold uppercase tracking-wide text-slate-500 sm:col-span-2">
-                2010 - Present
+                2022 - 2023
               </header>
               <div className="z-10 sm:col-span-6 ">
                 <h3 className="font-medium leading-snug text-slate-200 ">
@@ -78,38 +93,60 @@ const Experince = () => {
                   >
                     <span className="absolute -inset-x-4 -inset-y-2.5 hidden rounded md:-inset-x-6 md:-inset-y-4 lg:block"></span>
                     <span className="inline-block">
-                      Web Developer • Zuellig Pharma
+                      Amazon Product Research • Zephry Tech Co LLC
                     </span>
                   </a>
                 </h3>
 
                 <p className="mt-2 text-sm leading-normal">
-                  As a developer, I am tasked to create and develop websites and
-                  applications for the company. To achieve this goal, I mainly
-                  use React library and its framework — Nextjs. Additionally we
-                  also use WordPress to cater clients whom prefers to create
-                  their website on WordPress CMS. We are also venturing the
-                  theme and plugin development.
+                  For wholesale product research, our emphasis is on identifying
+                  existing suppliers offering products at the most competitive
+                  prices compared to Amazon sellers. We intend to sell these
+                  products on Amazon, targeting a profit of $3 and a 5% Return
+                  on Investment (ROI), aligning with our wholesale purchasing
+                  strategy. To enhance efficiency, we use Trello for task
+                  management, ensuring a minimum of 10 leads are generated per
+                  week.
                 </p>
                 <ul className="mt-2 flex flex-wrap">
                   <li className="mr-1.5 mt-2">
                     <div className="flex items-center bg-teal-400/10 rounded-full px-3 py-1 text-xs font-medium leading-5 text-teal-300">
-                      Javascript
+                      Seller Amp
                     </div>
                   </li>
                   <li className="mr-1.5 mt-2">
                     <div className="flex items-center bg-teal-400/10 rounded-full px-3 py-1 text-xs font-medium leading-5 text-teal-300">
-                      Javascript
+                      Google Sheet
                     </div>
                   </li>
                   <li className="mr-1.5 mt-2">
                     <div className="flex items-center bg-teal-400/10 rounded-full px-3 py-1 text-xs font-medium leading-5 text-teal-300">
-                      Javascript
+                      Keepa
                     </div>
                   </li>
                   <li className="mr-1.5 mt-2">
                     <div className="flex items-center bg-teal-400/10 rounded-full px-3 py-1 text-xs font-medium leading-5 text-teal-300">
-                      Javascript
+                      Helium 10
+                    </div>
+                  </li>
+                  <li className="mr-1.5 mt-2">
+                    <div className="flex items-center bg-teal-400/10 rounded-full px-3 py-1 text-xs font-medium leading-5 text-teal-300">
+                      SpreadSheet
+                    </div>
+                  </li>
+                  <li className="mr-1.5 mt-2">
+                    <div className="flex items-center bg-teal-400/10 rounded-full px-3 py-1 text-xs font-medium leading-5 text-teal-300">
+                      Amazon
+                    </div>
+                  </li>
+                  <li className="mr-1.5 mt-2">
+                    <div className="flex items-center bg-teal-400/10 rounded-full px-3 py-1 text-xs font-medium leading-5 text-teal-300">
+                      Trello
+                    </div>
+                  </li>
+                  <li className="mr-1.5 mt-2">
+                    <div className="flex items-center bg-teal-400/10 rounded-full px-3 py-1 text-xs font-medium leading-5 text-teal-300">
+                      WhatsApp
                     </div>
                   </li>
                 </ul>
@@ -120,7 +157,7 @@ const Experince = () => {
             <div className="group relative grid pb-1 transition-all sm:grid-cols-8 sm:gap-8 md:gap-4 lg:hover:!opacity-100 lg:group-hover/list:opacity-50">
               <div className="absolute -inset-x-4 -inset-y-4 z-0 hidden rounded-md transition motion-reduce:transition-none lg:-inset-x-6 lg:block lg:group-hover:bg-slate-800/50 lg:group-hover:shadow-[inset_0_1px_0_0_rgba(148,163,184,.0.1)] lg:group-hover:drop-shadow-lg"></div>
               <header className="z-10 mb-2 mt-1 text-xs font-semibold uppercase tracking-wide text-slate-500 sm:col-span-2">
-                2010 - Present
+                2021 - 2022
               </header>
               <div className="z-10 sm:col-span-6 ">
                 <h3 className="font-medium leading-snug text-slate-200 ">
@@ -129,91 +166,48 @@ const Experince = () => {
                     href=""
                   >
                     <span className="absolute -inset-x-4 -inset-y-2.5 hidden rounded md:-inset-x-6 md:-inset-y-4 lg:block"></span>
-                    <span className="inline-block">
-                      Web Developer • Zuellig Pharma
-                    </span>
+                    <span className="inline-block">Amazon VA • Pipkin LLC</span>
                   </a>
                 </h3>
 
                 <p className="mt-2 text-sm leading-normal">
-                  As a developer, I am tasked to create and develop websites and
-                  applications for the company. To achieve this goal, I mainly
-                  use React library and its framework — Nextjs. Additionally we
-                  also use WordPress to cater clients whom prefers to create
-                  their website on WordPress CMS. We are also venturing the
-                  theme and plugin development.
+                  To quickly find leads, I use manual sourcing, reverse
+                  sourcing, and storefront stalking. Seller Amp SAS helps by
+                  automatically listing items in Google Sheets. My criteria: at
+                  least $3 profit and 30% ROI per item with a minimum of 100
+                  monthly sales, or 20% ROI with 500 monthly sales. It&apos;s
+                  about striking the right balance for profitable and popular
+                  leads.
                 </p>
                 <ul className="mt-2 flex flex-wrap">
                   <li className="mr-1.5 mt-2">
                     <div className="flex items-center bg-teal-400/10 rounded-full px-3 py-1 text-xs font-medium leading-5 text-teal-300">
-                      Javascript
+                      Seller Amp
                     </div>
                   </li>
                   <li className="mr-1.5 mt-2">
                     <div className="flex items-center bg-teal-400/10 rounded-full px-3 py-1 text-xs font-medium leading-5 text-teal-300">
-                      Javascript
+                      Google Sheet
                     </div>
                   </li>
                   <li className="mr-1.5 mt-2">
                     <div className="flex items-center bg-teal-400/10 rounded-full px-3 py-1 text-xs font-medium leading-5 text-teal-300">
-                      Javascript
+                      Keepa
                     </div>
                   </li>
                   <li className="mr-1.5 mt-2">
                     <div className="flex items-center bg-teal-400/10 rounded-full px-3 py-1 text-xs font-medium leading-5 text-teal-300">
-                      Javascript
-                    </div>
-                  </li>
-                </ul>
-              </div>
-            </div>
-          </li>
-          <li className="mb-12">
-            <div className="group relative grid pb-1 transition-all sm:grid-cols-8 sm:gap-8 md:gap-4 lg:hover:!opacity-100 lg:group-hover/list:opacity-50">
-              <div className="absolute -inset-x-4 -inset-y-4 z-0 hidden rounded-md transition motion-reduce:transition-none lg:-inset-x-6 lg:block lg:group-hover:bg-slate-800/50 lg:group-hover:shadow-[inset_0_1px_0_0_rgba(148,163,184,.0.1)] lg:group-hover:drop-shadow-lg"></div>
-              <header className="z-10 mb-2 mt-1 text-xs font-semibold uppercase tracking-wide text-slate-500 sm:col-span-2">
-                2010 - Present
-              </header>
-              <div className="z-10 sm:col-span-6 ">
-                <h3 className="font-medium leading-snug text-slate-200 ">
-                  <a
-                    className="inline-flex items-baseline font-medium leading-tight text-slate-200 hover:text-teal-300 focus-visible:text-teal-300 group/link text-base"
-                    href=""
-                  >
-                    <span className="absolute -inset-x-4 -inset-y-2.5 hidden rounded md:-inset-x-6 md:-inset-y-4 lg:block"></span>
-                    <span className="inline-block">
-                      Web Developer • Zuellig Pharma
-                    </span>
-                  </a>
-                </h3>
-
-                <p className="mt-2 text-sm leading-normal">
-                  As a developer, I am tasked to create and develop websites and
-                  applications for the company. To achieve this goal, I mainly
-                  use React library and its framework — Nextjs. Additionally we
-                  also use WordPress to cater clients whom prefers to create
-                  their website on WordPress CMS. We are also venturing the
-                  theme and plugin development.
-                </p>
-                <ul className="mt-2 flex flex-wrap">
-                  <li className="mr-1.5 mt-2">
-                    <div className="flex items-center bg-teal-400/10 rounded-full px-3 py-1 text-xs font-medium leading-5 text-teal-300">
-                      Javascript
+                      SpreadSheet
                     </div>
                   </li>
                   <li className="mr-1.5 mt-2">
                     <div className="flex items-center bg-teal-400/10 rounded-full px-3 py-1 text-xs font-medium leading-5 text-teal-300">
-                      Javascript
+                      Amazon
                     </div>
                   </li>
                   <li className="mr-1.5 mt-2">
                     <div className="flex items-center bg-teal-400/10 rounded-full px-3 py-1 text-xs font-medium leading-5 text-teal-300">
-                      Javascript
-                    </div>
-                  </li>
-                  <li className="mr-1.5 mt-2">
-                    <div className="flex items-center bg-teal-400/10 rounded-full px-3 py-1 text-xs font-medium leading-5 text-teal-300">
-                      Javascript
+                      Gmail
                     </div>
                   </li>
                 </ul>
